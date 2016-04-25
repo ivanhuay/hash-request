f9adba506993f6a2e1665463f09d4b1b67b1c132
ivanhuay <ivan.huayrana@despegar.com>
2016-04-25 09:38:14 -0300

    simple version

--- app.js
+++ app.js
@@ -14,25 +14,21 @@ var returnMd5 = function(body) {
 };
 
 var httpCallback = function(jsonName) {
-
   return function(resp) {
     var body = "";
     var name = jsonName;
-    console.log("httpCallback for:" + jsonName);
     resp.on("data", function(data) {
       body += data;
     });
     resp.on("end", function() {
       _responseJSON[name] = returnMd5(body);
-      _requestPending--;
-      console.log("remove Pending");
-      console.log(_responseJSON[name]);
+      console.log(name+": "+_responseJSON[name]);
     });
   };
 };
 
 
-function readFiles(dirname, onFileContent, onError, finalCallback) {
+function readFiles(dirname, onFileContent, onError) {
   fs.readdir(dirname, function(err, filenames) {
     if (err) {
       onError(err);
@@ -46,33 +42,11 @@ function readFiles(dirname, onFileContent, onError, finalCallback) {
         }
         _requestPending++;
         onFileContent(filename, content);
-        console.log("inde: " + index + " length: " + filenames.length);
-        if (index == filenames.length - 1) {
-          exec(_requestPending,finalCallback);
-        }
       });
     });
   });
 }
 
-var timeCheck = null;
-
-
 readFiles(__dirname + "/sources", function(filename, content) {
   http.get(JSON.parse(content), httpCallback(filename.replace(".json", "")));
-}, console.log, function() {
-  console.log("final calback");
-
-  if (_requestPending.length !== 0) {
-    timeCheck = setTimeout(function() {
-      console.log("length: " + _requestPending.length);
-      if (_requestPending.length !== 0) {
-
-      } else {
-        console.log(JSON.stringify(_responseJSON));
-
-      }
-    }, 5000);
-  }
-
-});
+}, console.log);
