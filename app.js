b6d4a77b3ae3c0b298fd05169b349cd3c0b08c43
ivanhuay <ivanhuay@gmai.com>
2016-05-03 21:16:08 -0300

    multiple urls suport added

--- app.js
+++ app.js
@@ -17,44 +17,53 @@ var jsonRequest = function(url) {
         "method": "GET"
     };
 };
-var returnMd5 = function(body) {
+var returnMd5 = function(plainText) {
     var md5Sum = crypto.createHash("md5");
-    md5Sum.update(body);
+    md5Sum.update(plainText);
     var hash = md5Sum.digest("hex");
     return hash;
 };
 
-function getHashUrl(url, selector, callback) {
+var getHashUrl = function(url, selector, callback, index) {
+    if (url instanceof Array) {
+        for (var i in url) {
+            getHashUrl(url[i], selector, callback, i);
+        }
+        return;
+    }
+
     if (typeof selector == "function") {
         callback = selector;
         selector = null;
     }
-    var body = "";
-    var requestHandler = (/https/.test(url)) ? https : http;
-    var req = requestHandler.request(jsonRequest(url), function(resp) {
-        resp.on("data", function(data) {
-            body += data;
+
+    var body = "",
+        requestHandler = (/https/.test(url)) ? https : http,
+        req = requestHandler.request(jsonRequest(url), function(resp) {
+            resp.on("data", function(data) {
+                body += data;
+            });
+            resp.on('close', function() {
+                console.log("\n\nClose received!");
+            });
+            resp.on("end", function() {
+                var $ = cheerio.load(body);
+                var response = {
+                    body: returnMd5($("body").html()),
+                    head: returnMd5($("head").html()),
+                    statusCode: resp.statusCode,
+                    headers: resp.headers
+                };
+                if(typeof index !== "undefined") response.index = index;
+                if (selector !== null) response.selector = returnMd5($(selector).html());
+                callback(response);
+            });
         });
-        resp.on('close', function() {
-            console.log("\n\nClose received!");
-        });
-        resp.on("end", function() {
-            var $ = cheerio.load(body);
-            var response = {
-                body: returnMd5($("body").html()),
-                head: returnMd5($("head").html()),
-                statusCode: resp.statusCode,
-                headers: resp.headers
-            };
-            if (selector !== null) response.selector = returnMd5($(selector).html());
-            callback(response);
-        });
-    });
 
     req.on('error', function(e) {
         console.log('problem with request: ' + e.message);
     });
     req.end();
-}
+};
 
 exports.getHash = getHashUrl;
--- test/test.js
+++ test/test.js
@@ -24,3 +24,15 @@ describe("hash http request", function() {
         });
     });
 });
+
+describe("multiple url hash", function() {
+    describe("http simple multiple request", function() {
+        it("check status in multiple request", function() {
+            var hashes = ["912949687a6fe75350bf36928cf64b67", "61fa172f7c9ecdd884f589a9d89b725a", "f2c0c63aed3f8a71d47eec78e697f483"];
+            hashRequest.getHash(["httpd.apache.org/security_report.html", "https://www.npmjs.com", "https://nodejs.org"], function(response) {
+                expected(200).to.equal(response.statusCode);
+                expected(hashes[response.index]).to.equal(response.body);
+            });
+        });
+    });
+});
